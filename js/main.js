b70df37e26da9b9ea421df92b27c20d531d5d86e
Dufran <Zibertua@gmail.com>
2017-10-21 18:32:48 +0300

    toggles, dragable divs

---
+++ js/main.js
@@ -0,0 +1,251 @@
+$(function() {
+    $("#innerHoversewer, #innerHoverreef, #innerHovertropical, #innerHovervaalcity, #innerHovervaultofatziri, #innerHovercanyon, #innerHoverburial, #innerHoverbeach, #innerHoverjungle, #innerHovermarshes, #innerHoverpyramid, #innerHoverprimordal, #innerHoverphantasmagoria, #innerHovercavern, #innerHoverwaste, #innerHovermessa, #innerHoverquay, #innerHoverquary, #innerHoveracidlake, #innerHoverarachtomb, #innerHoverbeach, #innerHoverActon, #innerHoverArsenal, #innerHoverBazaar, #innerHoverMaze, #innerHoverPlaza, #innerHoverShipyard, #innerHoverArmoury,#innerHoverAbyss,#innerHoverAcademy, #innerHoverArena, #innerHoverArcade, #innerHoverActon, #innerHoverAshenWood, #innerHoverRamparts, #innerHoverPromenade, #innerHoverPrecinct, #innerHoverOasis, #innerHoverAridlake, #innerHoverDesert, #innerHoverColonnade, #innerHoverOrchard, #innerhoverTemple, #innerHoverMalformation, #innerHoverExcavation, #innerHoverNecropolis, #innerHoverLair, #innerHoverMineralPools, #innerHoverShrine, #innerHoverGorge, #innerHoverVault, #innerHoverUndergroundSea, #innerHoverUndergroundRiver, #innerHoverCoves, #innerHoverIvoryTemple, #innerHoverColosseum, #innerHoverPlateau, #innerHoverResidence, #innerHoverTortureChamber, #innerHoverCatacombs, #innerHoverWaterways, #innerHoverSprings, #innerHoverOvergrownRuin, #innerHoverPalace, #innerHoverDarkForest, #innerHoverHighGarden, #innerHoverTerrace, #innerHoverBarrows, #innerHoverMuseum, #innerHoverCourtyard, #innerHoverWasteland, #innerHoverCrematorium, #innerHoverSulphurWastes, #innerHoverVolcano, #innerHoverCore, #innerHoverCrypt, #innerHoverCemetery, #innerHoverBog, #innerHoverChateau, #innerHoverEstuary, #innerHoverBeacon, #innerHoverHallofGrandmasters, #innerhoverperandusmanor, #innerHoverpit, #innerHoverScriptorium, #innerHovercrystalore, #innerHoverFactory, #innerHoverchanell, #innerHoverracecourse, #innerHovercells, #innerHoverArachnidNest, #innerHoverShore, #innerHoverAtoll, #innerHoverGhetto, #innerHoverGraveyard, #innerHoverTower, #innerHoverSpiderLair, #innerHoverThicket, #innerHoverSpiderForest, #innerHoverMudGeyser, #innerHoverPier, #innerHoverOvergrownShrine, #innerHoverPeninsula, #innerHoverWharf, #innerHoverStrand, #innerHoverCastleRuins, #innerHoverDunes, #innerHoverDungeon, #innerHoverVilla, #innerHoverGroto    ").draggable({ scroll: true });
+
+
+});
+
+
+function hide() {
+    var y = document.getElementsByClassName("T2");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T3");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T4");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T5");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T6");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T7");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T8");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T9");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T10");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T11");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T12");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T12");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T13");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T14");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T15");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    var y = document.getElementsByClassName("T1");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+}
+
+function shape1() {
+    var y = document.getElementsByClassName("shap1");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape2() {
+    var y = document.getElementsByClassName("shap2");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape3() {
+    var y = document.getElementsByClassName("shap3");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape4() {
+    var y = document.getElementsByClassName("shap4");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape5() {
+    var y = document.getElementsByClassName("shap5");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape6() {
+    var y = document.getElementsByClassName("shap6");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape7() {
+    var y = document.getElementsByClassName("shap7");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape8() {
+    var y = document.getElementsByClassName("shap8");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape9() {
+    var y = document.getElementsByClassName("shap9");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function shape10() {
+    var y = document.getElementsByClassName("shap10");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function T1() {
+    var y = document.getElementsByClassName("T1");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+
+
+
+function T2() {
+    var y = document.getElementsByClassName("T2");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "block";
+    }
+}
+
+function T3() {
+    var b = document.getElementsByClassName("T3");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T4() {
+    var b = document.getElementsByClassName("T4");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T5() {
+    var b = document.getElementsByClassName("T5");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T6() {
+    var b = document.getElementsByClassName("T6");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T7() {
+    var b = document.getElementsByClassName("T7");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T8() {
+    var b = document.getElementsByClassName("T8");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T9() {
+    var b = document.getElementsByClassName("T9");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T10() {
+    var b = document.getElementsByClassName("T10");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T11() {
+    var b = document.getElementsByClassName("T11");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T12() {
+    var b = document.getElementsByClassName("T12");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T13() {
+    var b = document.getElementsByClassName("T13");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T14() {
+    var b = document.getElementsByClassName("T14");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
+
+function T15() {
+    var b = document.getElementsByClassName("T15");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.display = "block";
+    }
+}
